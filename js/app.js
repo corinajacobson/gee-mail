cb4b70e7e3da59d2e59198f579cac39f5a53318a
Corina <corina.aj@gmail.com>
2015-10-04 17:15:37 -1000

    formatting better
    javascript still not working

--- js/app.js
+++ js/app.js
@@ -1,27 +1,10 @@
 var main = function() {
-        $('.email').click(function() {
-        $('.email').removeClass('.current');
-        $('.email-content').hide();
+    $('.email').click(function() {
+    $('.email').removeClass('current');
+    $('.email-content').hide();
 
-        $(this).addClass('current');
-        $(this).children('email-content').show();
-        });
+    $(this).addClass('current');
+    $(this).children('.email-content').show();
 
-    $(document).keypress(function(event) {
-    	if(event.which === 111) {
-      		$('.email-content').hide();
-      		$('.current').children('.email-content').show();
-  		  }
-
-   		else if(event.which === 110) {
-      		var currentEmail = $('.current');
-      		var nextEmail = currentEmail.next();
-      
-      		currentEmail.removeClass('current');
-      		nextEmail.addClass('current');
-    	}
- 	 });
-
-}
-
-        	$(document).ready(main);
+});
+$(document).ready(main);
